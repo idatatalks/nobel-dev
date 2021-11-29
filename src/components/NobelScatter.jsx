e83ad2b9b28ff5583365e760fc42ea46719d9aaf
Yuan You <macyou@gmail.com>
2021-11-29 15:50:03 -0500

    adjust y axis

--- src/components/NobelScatter.jsx
+++ src/components/NobelScatter.jsx
@@ -31,26 +31,53 @@ export const NobelScatter = (props) => {
   const matches = useMediaQuery("(min-width:900px)");
   const width = matches ? "100%" : 600;
 
-  const shapeProps = {
+  const mark = {
     size: 5,
+    width: 10,
+    height: 10,
   };
+
   const horizontalGap = 10;
+  const margins = {
+    top: 20,
+    right: 20,
+    bottom: 80,
+    left: 30,
+  };
+  const horPaddings = { left: 10, right: 20 };
+  const vertPaddings = { top: 10, bottom: 10 };
 
   const filterData = () => {
     const data = props.data;
     console.log(data);
     const filteredData = d3
       .flatGroup(data, (d) => d.country)
-      .map((d, id) => {
-        return d[1].map((d, i) => {
-          d.winnerId = i + 1;
-          d.countryId = id + 1;
+      .map((d, i) => {
+        return d[1].map((d, j) => {
+          d.winnerId = j + 1;
+          d.winnerY = d.winnerId * mark.height;
+          d.countryId = i + 1;
           return d;
         });
       })
       .flat();
 
-    filteredData.forEach((d) => (d.winnerId *= 2 * shapeProps.size));
+    filteredData.winnerNum = (() => {
+      return d3
+        .flatGroup(data, (d) => d.country)
+        .map(([key, value]) => {
+          return [key, value.length];
+        });
+    })();
+
+    filteredData.maxWinners = (() => {
+      let index = d3.maxIndex(filteredData.winnerNum, (d) => d[1]);
+      return [
+        filteredData.winnerNum[index][0],
+        filteredData.winnerNum[index][1],
+      ];
+    })();
+    console.log("xxx:", filteredData.maxWinners);
     filteredData.countryNum = filteredData[filteredData.length - 1].countryId;
     filteredData.countries = Array.from(
       d3.group(data, (d) => d.country).keys()
@@ -58,6 +85,7 @@ export const NobelScatter = (props) => {
     console.log("Country numbers:", filteredData.countryNum);
     console.log("Country names:", filteredData.countries.length);
     console.log("filtered data:", filteredData);
+
     setData({ data: filteredData, isDataMangled: true });
     return filteredData;
   };
@@ -71,6 +99,20 @@ export const NobelScatter = (props) => {
     console.log("Data is NOT processed yet.");
     return "";
   }
+
+  const chartWidth = () =>
+    mark.width * 2 * data.countries.length +
+    margins.left +
+    margins.right +
+    horPaddings.left +
+    horPaddings.right;
+
+  const chartHeight = () =>
+    mark.height * 1.2 * data.maxWinners[1] +
+    margins.top +
+    margins.bottom +
+    vertPaddings.top +
+    vertPaddings.bottom;
 
   return (
     <div
@@ -86,17 +128,9 @@ export const NobelScatter = (props) => {
       >
         Hello world
       </FlexText>
-      <Box
-        sx={{
-          width: { md: 300, lg: 600 },
-          fontSize: { md: 16, lg: 30 },
-          color: { md: "red", lg: "green" },
-        }}
-      >
-        hello world
-      </Box> */}
-      <ResponsiveContainer width={shapeProps.size * 8 * data.countries.length}>
-        <ScatterChart margin={{ top: 20, right: 20, bottom: 80, left: 30 }}>
+      */}
+      <ResponsiveContainer width={chartWidth()} height={chartHeight()}>
+        <ScatterChart margin={margins}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis
             allowDuplicatedCategory={false}
@@ -108,7 +142,7 @@ export const NobelScatter = (props) => {
             angle={90}
             dx={-50}
             interval={0}
-            padding={{ left: 10, right: 20 }}
+            padding={horPaddings}
             allowDataOverflow={false}
             tickCount={data.countries.length}
             domain={["dataMin", "dataMax"]}
@@ -119,13 +153,13 @@ export const NobelScatter = (props) => {
             name="number"
             domain={["auto", "dataMax"]}
             dy={0}
-            padding={{ top: 10, bottom: 5 }}
+            padding={vertPaddings}
             interval={0}
-            tickCount={10}
+            tickCount={data.maxWinners[1]}
             // domain={["dataMin-10", "dataMax+10"]}
           />
           {/* <ZAxis dataKey="year" range={[64, 144]} name="year" /> */}
-          <Legend verticalAlign="top" margin={{ bottom: 100 }} />
+          <Legend verticalAlign="top" margin={{ top: 0 }} />
           <Tooltip cursor={{ strokeDasharray: "3 3" }} />
           <Scatter
             shape="cross"
@@ -145,7 +179,7 @@ export const NobelScatter = (props) => {
 
 const CustomizedAxisTick = (props) => {
   const { x, y, payload, data, index } = props;
-  console.log("YYY:", props);
+  // console.log("YYY:", props);
   // const xlabels = Array.from(data.countries);
   // console.log(xlabels);
   // console.log(payload.value - 1, xlabels[payload.value - 1]);
@@ -167,10 +201,10 @@ const CustomizedAxisTick = (props) => {
 };
 
 const CustomizedScatterShape = (props) => {
-  const { x, y, winnerId, countryId, interval, payload } = props;
+  const { x, y, winnerId, winnerY, countryId, interval, payload } = props;
   // console.log("YYY:", props);
   return (
-    <g transform={`translate(${x},${y - winnerId})`}>
+    <g transform={`translate(${x},${y})`}>
       <circle cx={5} cy={5} r={5} fill="orange" />
     </g>
   );
